75b1d2e03b141affdb90d4f51a7a946f03fdfd6c
e.mortoray <e.mortoray@ecircle.com>
2008-10-15 07:40:53 +0000

    final step before removing forms

--- test/edit.php
+++ test/edit.php
@@ -1,14 +1,22 @@
 <?php
-	require_once 'gen/SampleFormQuick.form.inc';
+	require_once 'persephone/html_quickform.inc';
 	require_once 'common_test.inc';
 	
 	std_header();
 	
-	dbs_standard_form(
-		new FormSampleFormQuick(),
-		'ID',
-		array( ) 
-		)->execute();
-		
+class EditForm extends DBS_FormBase_QuickForm {
+	public $ENTITY = 'DBSTest';
+	
+	protected function _setup() {
+		parent::_setup();
+		$this->addStaticElement( 'id' );
+		$this->addTypicalElement( 'name' );
+		$this->addTypicalElement( 'date' );
+		$this->addStaticElement( 'float' );
+	}
+}
+
+	dbs_standard_form( new EditForm(), 'ID' )->execute();
+	
 	print( "<p><a href='listing.php'>Return to listing</a></p>" );
 ?>
--- test/edit_full.php
+++ test/edit_full.php
@@ -1,14 +1,22 @@
 <?php
-	require_once 'gen/SampleFormFull.form.inc';
+	require_once 'persephone/html_quickform.inc';
 	require_once 'common_test.inc';
 	
 	std_header();
 	
-	dbs_standard_form(
-		new FormSampleFormFull(),
-		'ID',
-		array( ) 
-		)->execute();
+class EditForm extends DBS_FormBase_QuickForm {
+	public $ENTITY = 'DBSTest';
+	
+	protected function _setup() {
+		parent::_setup();
+		$this->addStaticElement( 'id' );
+		foreach( array( 'name', 'date', 'time', 'dateTime', 'bool', 'decimal', 'float', 'memoryOnly', 'otherDecimal', 'nullStr' ) as $element )
+			$this->addTypicalElement( $element );
+		$this->addSelectElement( 'nameRef', 	DBSName::search( DBS_Query::matchAll() ) );
+	}
+}
+	
+	dbs_standard_form( new EditForm(), 'ID' )->execute();
 		
 	print( "<p><a href='listing.php'>Return to listing</a></p>" );
 ?>
